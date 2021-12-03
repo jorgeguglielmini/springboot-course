3410b82145ad599b53a6bd346ef8c1d21f3ecfce
Jorge Guglielmini <jorge.guglielmini@contmatic.com.br>
2021-12-03 15:30:47 -0300

    oderStatus

--- src/main/java/com/guglielminijorge/course/config/TestConfig.java
+++ src/main/java/com/guglielminijorge/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.guglielminijorge.course.entities.Order;
 import com.guglielminijorge.course.entities.User;
+import com.guglielminijorge.course.entities.enums.OrderStatus;
 import com.guglielminijorge.course.repositories.OrderRepository;
 import com.guglielminijorge.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null,"maria","maria@gmail.com","123456789","123564");
 		User u2 = new User(null,"Joao","joao@gmail.com","987456321","563214");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYNENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYNENT, u1);
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
