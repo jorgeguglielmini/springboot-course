ff5aedc10f82955160067771de14c0195a793a36
Jorge Guglielmini <jorge.guglielmini@contmatic.com.br>
2021-12-09 12:04:40 -0300

    many to many with join table

--- src/main/java/com/guglielminijorge/course/entities/Category.java
+++ src/main/java/com/guglielminijorge/course/entities/Category.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_category")
@@ -21,6 +23,8 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {}
@@ -47,7 +51,6 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
-	@Transient
 	public Set<Product> getProducts() {
 		return products;
 	}
